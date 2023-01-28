7c072b79d76f54b15c984737d27c82ca41884f5c
Anurag8112 <katiyaranu2003@gmail.com>
2023-01-28 18:24:46 +0530

    Color API

--- Ecommerce/Repository/ColorRepository.cs
+++ Ecommerce/Repository/ColorRepository.cs
@@ -4,6 +4,7 @@ using Ecommerce.Models.ViewModel;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Ecommerce.Repository
 {
@@ -42,17 +43,81 @@ namespace Ecommerce.Repository
 
         public bool DeleteColor(DeleteColorModel model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                EcommerceContext db = new EcommerceContext();
+                var Color = db.Colors.FirstOrDefault(x => x.Id == model.Id);
+
+                if (Color == null)
+                {
+                    logger.LogInformation("-----Invalid Color Id-----");
+                    throw new Exception("Invalid Color Id");
+                }
+                db.Colors.Remove(Color);
+                db.SaveChanges();
+                logger.LogInformation("-----Color Deleted-----");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.InnerException.ToString());
+                throw new Exception(ex.InnerException.ToString());
+            }
         }
 
         public bool EditColor(EditColorModel model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                EcommerceContext db = new EcommerceContext();
+                var Color = db.Colors.FirstOrDefault(x => x.Id == model.Id);
+
+                if (Color == null)
+                {
+                    logger.LogError("------Invalid Color Id-------");
+                    throw new Exception("Invalid Color Id");
+                }
+
+                Color.Color1 = model.Color;
+
+                db.Colors.Add(Color);
+                db.SaveChanges();
+                logger.LogInformation("-----Color Edited Successfully-----");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.InnerException.ToString());
+                throw new Exception(ex.InnerException.ToString());
+            }
         }
 
         public List<ShowAllColor> ShowAllColor()
         {
-            throw new NotImplementedException();
+            try
+            {
+                EcommerceContext db = new EcommerceContext();
+                List<ShowAllColor> ColorList = new List<ShowAllColor>();
+
+                foreach (var color in db.Colors)
+                {
+                    var colors = new ShowAllColor()
+                    {
+                        Id = color.Id,
+                        Color = color.Color1
+                    };
+
+                    ColorList.Add(colors);
+                }
+
+                logger.LogInformation("-----Color Added to List-----");
+                return ColorList;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.InnerException.ToString());
+                throw new Exception(ex.InnerException.ToString());
+            }
         }
     }
 }
